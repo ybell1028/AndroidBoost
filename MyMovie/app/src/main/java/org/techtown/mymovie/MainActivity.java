4dee76a889a2dffce8ca3af069f4f65df524d34e
sufferingcode <ybell1028@gmail.com>
2019-10-07 09:56:33 +0900

    191016

--- MyMovie/app/src/main/java/org/techtown/mymovie/MainActivity.java
+++ MyMovie/app/src/main/java/org/techtown/mymovie/MainActivity.java
@@ -4,14 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button thumbUpButton, thumbDownButton;
+    TextView thumbUpNum, thumbDownNum;
+
+    int good = 0, bad = 0; // 좋아요 싫어요의 수
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        thumbUpButton = (Button) findViewById(R.id.thumb_up_button);
+        thumbDownButton= (Button) findViewById(R.id.thumb_down_button);
+
+        thumbUpNum = (TextView)(findViewById(R.id.thumb_up_num));
+        thumbDownNum = (TextView)(findViewById(R.id.thumb_down_num));
+
+        thumbUpNum.setText(String.valueOf(good));
+        thumbUpNum.setText(String.valueOf(bad));
+
+        thumbUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                good++;
+                thumbUpNum.setText(String.valueOf(good));
+            }
+        });
+
+        thumbDownButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bad++;
+                thumbDownNum.setText(String.valueOf(bad));
+            }
+        });
     }
 
 }
