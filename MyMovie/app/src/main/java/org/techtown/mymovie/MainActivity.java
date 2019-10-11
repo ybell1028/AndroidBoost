ac47bb0c567a0b3591a00c93ce88451806759864
Jong Hun Hwang <ybell1028@gmail.com>
2019-10-12 00:40:08 +0900

    commentLayout

--- MyMovie/app/src/main/java/org/techtown/mymovie/MainActivity.java
+++ MyMovie/app/src/main/java/org/techtown/mymovie/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     TextView thumbUpNum, thumbDownNum;
 
     int good = 0, bad = 0; // 좋아요 싫어요의 수
+    boolean goodState = false;
+    boolean badState = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,27 +25,68 @@ public class MainActivity extends AppCompatActivity {
         thumbUpButton = (Button) findViewById(R.id.thumb_up_button);
         thumbDownButton= (Button) findViewById(R.id.thumb_down_button);
 
-        thumbUpNum = (TextView)(findViewById(R.id.thumb_up_num));
-        thumbDownNum = (TextView)(findViewById(R.id.thumb_down_num));
-
-        thumbUpNum.setText(String.valueOf(good));
-        thumbUpNum.setText(String.valueOf(bad));
-
         thumbUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                good++;
+                if(goodState){
+                    decrGood();
+                } else {
+                    incrGood();
+                }
                 thumbUpNum.setText(String.valueOf(good));
+
+                goodState = !goodState;
             }
         });
 
         thumbDownButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                bad++;
+                if(badState){
+                    decrBad();
+                } else {
+                    incrBad();
+                }
                 thumbDownNum.setText(String.valueOf(bad));
+
+                badState = !badState;
             }
         });
+
+        thumbUpNum = (TextView)(findViewById(R.id.thumb_up_num));
+        thumbDownNum = (TextView)(findViewById(R.id.thumb_down_num));
+
+        thumbUpNum.setText(String.valueOf(good));
+        thumbUpNum.setText(String.valueOf(bad));
+
+
     }
 
+    public void incrGood()
+    {
+        good++;
+        thumbUpNum.setText(String.valueOf(good));
+        thumbUpButton.setBackgroundResource(R.drawable.ic_thumb_up_selected);
+    }
+
+    public void decrGood()
+    {
+        good--;
+        thumbUpNum.setText(String.valueOf(good));
+        thumbUpButton.setBackgroundResource(R.drawable.ic_thumb_up);
+    }
+
+    public void incrBad()
+    {
+        bad++;
+        thumbDownNum.setText(String.valueOf(bad));
+        thumbDownButton.setBackgroundResource(R.drawable.ic_thumb_down_selected);
+    }
+
+    public void decrBad()
+    {
+        bad--;
+        thumbDownNum.setText(String.valueOf(bad));
+        thumbDownButton.setBackgroundResource(R.drawable.ic_thumb_down);
+    }
 }
